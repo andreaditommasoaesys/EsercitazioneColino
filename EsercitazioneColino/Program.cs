1e0285976d258ae03437c94d162ad88052b26d0f
andreaditommasoaesys <110617222+andreaditommasoaesys@users.noreply.github.com>
2022-08-04 17:38:15 +0200

    SecondoCommit

--- EsercitazioneColino/Program.cs
+++ EsercitazioneColino/Program.cs
@@ -4,9 +4,26 @@
     {
         static void Main(string[] args)
         {
-            var rowNumber = Convert.ToInt32(Console.ReadLine());
+
+            int rowNumber;
+            bool control=Int32.TryParse(Console.ReadLine(), out  rowNumber);
+            if (control)
+            {
+                if (rowNumber >= 10 & rowNumber <= 20)
+                {
+                    CreateFile(rowNumber);
+                }
+            }
             
             Console.ReadKey();
         }
+        public static bool CreateFile(int rownumber)
+        {
+            bool control=false;
+            string path = @"..\OperazioniAPI.txt";
+            StreamWriter sw = File.CreateText(path);
+            return control;
+        }
+        
     }
 }
