c3832c5bba94e2681548b41bcbd9eb602e2861fc
Andrea Di Tommaso <andrea.ditommaso@aesys.tech>
2022-08-04 16:43:30 +0200

    FirstCommit

---
+++ EsercitazioneColino/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EsercitazioneColino
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rowNumber = Convert.ToInt32(Console.ReadLine());
+            
+            Console.ReadKey();
+        }
+    }
+}
