2fbbdfc1d9b04fda75b3dbd3c0dc04655904b6e2
SarlanVili <simone.patricelli@aesys.tech>
2022-08-04 17:51:06 +0200

    Created a check on input number

--- EsercitazioneColino/Program.cs
+++ EsercitazioneColino/Program.cs
@@ -4,26 +4,33 @@
     {
         static void Main(string[] args)
         {
-
             int rowNumber;
-            bool control=Int32.TryParse(Console.ReadLine(), out  rowNumber);
-            if (control)
+            bool control = false;
+            while (!control)
             {
+                Console.WriteLine("Please enter a valid number, between 10 and 20");
+                control = Int32.TryParse(Console.ReadLine(), out rowNumber);
+
                 if (rowNumber >= 10 & rowNumber <= 20)
                 {
                     CreateFile(rowNumber);
+                    control = true;
                 }
+                else
+                {
+                    control = false;
+                }
+                
             }
-            
             Console.ReadKey();
         }
         public static bool CreateFile(int rownumber)
         {
-            bool control=false;
+            bool control = false;
             string path = @"..\OperazioniAPI.txt";
             StreamWriter sw = File.CreateText(path);
             return control;
         }
-        
+
     }
 }
